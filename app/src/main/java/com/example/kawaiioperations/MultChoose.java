3a2f1f01d50c958ba88472cee7897aae2b63a151
Melo Abraao <abraao.st.melo@gmail.com>
2022-10-06 23:27:44 -0300

    Multiple choice numbers added and some improvements in layout

--- app/src/main/java/com/example/kawaiioperations/MultChoose.java
+++ app/src/main/java/com/example/kawaiioperations/MultChoose.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import java.util.ArrayList;
 
 public class MultChoose extends AppCompatActivity {
 
     private Button btn_two;
+    private CheckBox cbx1, cbx2,cbx3, cbx4, cbx5, cbx6, cbx7, cbx8, cbx9;
+    ArrayList<Integer> numValuesFamily = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +22,57 @@ public class MultChoose extends AppCompatActivity {
         setContentView(R.layout.activity_mult_choose);
 
         btn_two = findViewById(R.id.btn_twomult);
+        cbx1 = findViewById(R.id.cbx_n1);
+        cbx2 = findViewById(R.id.cbx_n2);
+        cbx3 = findViewById(R.id.cbx_n3);
+        cbx4 = findViewById(R.id.cbx_n4);
+        cbx5 = findViewById(R.id.cbx_n5);
+        cbx6 = findViewById(R.id.cbx_n6);
+        cbx7 = findViewById(R.id.cbx_n7);
+        cbx8 = findViewById(R.id.cbx_n8);
+        cbx9 = findViewById(R.id.cbx_n9);
+
         btn_two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intentTrivia = new Intent(getApplicationContext(), OperationsTriviaMult.class);
-                ArrayList<Integer> numValuesFamily = new ArrayList<Integer>();
-                numValuesFamily.add(8);
+                //numValuesFamily.add(8);
+                numValuesFamily = checkBoxValidation();
                 intentTrivia.putExtra("numFamily", numValuesFamily);
                 startActivity(intentTrivia);
             }
         });
+    }
+
+    public ArrayList<Integer> checkBoxValidation(){
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        if (cbx1.isChecked()){
+            array.add(1);
+        }
+        if (cbx2.isChecked()){
+            array.add(2);
+        }
+        if (cbx3.isChecked()){
+            array.add(3);
+        }
+        if (cbx4.isChecked()){
+            array.add(4);
+        }
+        if (cbx5.isChecked()){
+            array.add(5);
+        }
+        if (cbx6.isChecked()){
+            array.add(6);
+        }
+        if (cbx7.isChecked()){
+            array.add(7);
+        }
+        if (cbx8.isChecked()){
+            array.add(8);
+        }
+        if (cbx9.isChecked()){
+            array.add(9);
+        }
+        return (array);
     }
 }
--- app/src/main/java/com/example/kawaiioperations/OperationsTriviaMult.java
+++ app/src/main/java/com/example/kawaiioperations/OperationsTriviaMult.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -20,7 +21,7 @@ public class OperationsTriviaMult extends AppCompatActivity {
     private String resultEDT;
     private Button btnResultgo;
     private EditText etResult;
-    Random random = new Random();
+    Random randomNum = new Random();
 
 
 
@@ -31,47 +32,81 @@ public class OperationsTriviaMult extends AppCompatActivity {
 
         Intent intent = getIntent();
         ArrayList<Integer> arrayF11 = intent.getIntegerArrayListExtra("numFamily");
-        int numF1 = arrayF11.get(0);
+        tv_factor1 = findViewById(R.id.tv_factor1);
+        tv_factor2 = findViewById(R.id.tv_factor2);
+        tv_result = findViewById(R.id.tv_result);
+
+        int numF1 = RandArray(arrayF11);
+
+
         //int numF1 = intent.getIntExtra("numFamily",0);
 
         factor1 = numF1;
-        factor2 = random.nextInt(10);
+        factor2 = randomNum.nextInt(10);
         result = factor1*factor2;
 
-        tv_factor1 = findViewById(R.id.tv_factor1);
         tv_factor1.setText(factor1.toString());
 
-        tv_factor2 = findViewById(R.id.tv_factor2);
         tv_factor2.setText(factor2.toString());
 
-        tv_result = findViewById(R.id.tv_result);
-        tv_result.setText(result.toString());
-        tv_result.setVisibility(View.INVISIBLE);
+//        tv_result.setText(result.toString());
+//        tv_result.setVisibility(View.INVISIBLE);
 
         etResult = findViewById(R.id.et_result);
         etResult.requestFocus();
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         btnResultgo = findViewById(R.id.btn_resultgo);
-        btnResultgo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                resultEDT = etResult.getText().toString();
-                if (Integer.parseInt(resultEDT)==result){
-                    Toast.makeText(getApplicationContext(),"Correct",Toast.LENGTH_SHORT).show();
-                    Intent intent = getIntent();
-                    finish();
-                    startActivity(intent);
-                }else{
-                Toast.makeText(getApplicationContext(),"Try again", Toast.LENGTH_SHORT).show();
-                Intent intent = getIntent();
-                finish();
-                startActivity(intent);
-                }
 
-            }
-        });
 
+            btnResultgo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String edittext = etResult.getText().toString();
+
+                    if (TextUtils.isEmpty(edittext)) {
+                        etResult.setError("No Empty >.<'");
+                        return;
+
+                    } else {
+
+                        resultEDT = etResult.getText().toString();
+
+                        if (Integer.parseInt(resultEDT) == result) {
+
+                            Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+
+                            factor1 = RandArray(arrayF11);
+                            factor2 = randomNum.nextInt(10);
+                            result = factor1 * factor2;
+
+                            tv_factor1.setText(factor1.toString());
+                            tv_factor2.setText(factor2.toString());
+                            etResult.getText().clear();
+
+                        } else {
+
+                            Toast.makeText(getApplicationContext(), "Try again", Toast.LENGTH_SHORT).show();
+                            //factor1 = numF1;
+                            factor1 = RandArray(arrayF11);
+                            factor2 = randomNum.nextInt(10);
+                            result = factor1 * factor2;
+
+                            tv_factor1.setText(factor1.toString());
+                            tv_factor2.setText(factor2.toString());
+                            etResult.getText().clear();
+                        }
+
+                    }
+                } //END onClick
+            }); //END SET ON CLICK LISTENER
+        }
+
+
+    public Integer RandArray(ArrayList<Integer> array){
+        int rnd = new Random().nextInt(array.toArray().length);
+        //Toast.makeText(this,""+array.size(),Toast.LENGTH_SHORT).show();
+        return array.get(rnd);
     }
 
 }
